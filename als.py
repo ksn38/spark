ae6334e4f6b6249784dd56bc49107f41c675fdee
ksn38 <ksn38@yandex.ru>
2021-05-06 19:13:55 +0300

    -

--- als.py
+++ als.py
@@ -45,30 +45,12 @@ data = data.\
     
 data = data.withColumn('quantity', F.when(F.col("quantity") != 1, 1).otherwise(F.col("quantity")))
 
-# Count the total number of data in the dataset
-numerator = data.select("quantity").count()
-print('numerator', numerator)
-
-# Count the number of distinct userIds and distinct itemIds
-num_users = data.select("user_id").distinct().count()
-num_items = data.select("item_id").distinct().count()
-print('num_users', num_users)
-print('num_items', num_items)
-
-# Set the denominator equal to the number of users multiplied by the number of items
-denominator = num_users * num_items
-print('denominator', denominator)
-
-# Divide the numerator by the denominator
-sparsity = (1.0 - (numerator *1.0)/denominator)*100
-print("The data dataframe is ", "%.2f" % sparsity + "% empty.")
-
 # Create test and train set
 train = data.select('*').where(col('week_no') < 95)
 test = data.select('*').where(col('week_no') >= 95)
 
 # Create ALS model
-als = ALS(userCol="user_id", itemCol="item_id", ratingCol="quantity", nonnegative = True, implicitPrefs = True, coldStartStrategy="drop")
+als = ALS(maxIter=20, rank = 20, userCol="user_id", itemCol="item_id", ratingCol="quantity", nonnegative = True, implicitPrefs = True, coldStartStrategy="drop")
 
 t1 = time.time()
 
@@ -77,28 +59,19 @@ evaluator = RegressionEvaluator(metricName="rmse", labelCol="quantity", predicti
 model = als.fit(train)
 
 print('time', time.time() - t1)
-
-# Complete the code below to extract the ALS model parameters
-print("Model")
-
-# # Print "Rank"
-print("  Rank:", model._java_obj.parent().getRank())
-
-# Print "MaxIter"
-print("  MaxIter:", model._java_obj.parent().getMaxIter())
-
-# Print "RegParam"
-print("  RegParam:", model._java_obj.parent().getRegParam())
+print("Rank:", model._java_obj.parent().getRank())
+print("MaxIter:", model._java_obj.parent().getMaxIter())
+print("RegParam:", model._java_obj.parent().getRegParam())
 
 # View the predictions
 test_predictions = model.transform(test)
 RMSE = evaluator.evaluate(test_predictions)
 print(RMSE)
 
-nrecommendations = model.recommendForAllUsers(10)
+'''nrecommendations = model.recommendForAllUsers(10)
 nrecommendations.limit(10).show()
 
 nrecommendations = nrecommendations\
     .withColumn("rec_exp", explode("recommendations"))\
     .select('user_id', col("rec_exp.item_id"), col("rec_exp.rating"))
-nrecommendations.limit(10).show()
+nrecommendations.limit(10).show()'''
