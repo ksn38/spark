98cdfbac6f7486facb9c25df5d5637ddb822b571
ksn38 <ksn38@yandex.ru>
2021-05-09 12:58:47 +0300

    -

--- trained_als_with_stream.py
+++ trained_als_with_stream.py
@@ -1,22 +1,22 @@
-/kafka/bin/kafka-topics.sh --create --topic titanic --zookeeper 10.0.0.6:2181 --partitions 1 \
---replication-factor 2 --config retention.ms=-1
+#/kafka/bin/kafka-topics.sh --create --topic als_kafka --zookeeper 10.0.0.6:2181 --partitions 1 --replication-factor 2 --config retention.ms=-1
 
-/kafka/bin/kafka-console-producer.sh --topic titanic --broker-list 10.0.0.6:6667
+#/kafka/bin/kafka-console-producer.sh --topic als_kafka --broker-list 10.0.0.6:6667
 
-/kafka/bin/kafka-console-consumer.sh --topic titanic --bootstrap-server 10.0.0.6:6667
-
-
-export SPARK_KAFKA_VERSION=0.10
-/spark2.4/bin/pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.2 --driver-memory 512m --driver-cores 1 --master local[1]
+#/kafka/bin/kafka-console-consumer.sh --topic als_kafka --bootstrap-server 10.0.0.6:6667
 
 
-from pyspark.ml import Pipeline, PipelineModel
+#export SPARK_KAFKA_VERSION=0.10
+#/spark2.4/bin/pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.2 --driver-memory 512m --driver-cores 1 --master local[1]
+
+
+#from pyspark.ml import Pipeline, PipelineModel
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StringType, IntegerType, TimestampType, FloatType
 from pyspark.sql import functions as F
-from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler, CountVectorizer, StringIndexer, IndexToString
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+#from pyspark.ml.classification import LogisticRegression
+#from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler, CountVectorizer, StringIndexer, IndexToString
+#from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.recommendation import ALSModel
 
 spark = SparkSession.builder.appName("ksn38_spark").getOrCreate()
 
@@ -26,14 +26,14 @@ kafka_brokers = "10.0.0.6:6667"
 als_data = spark.readStream. \
     format("kafka"). \
     option("kafka.bootstrap.servers", kafka_brokers). \
-    option("subscribe", "titanic"). \
+    option("subscribe", "als_kafka"). \
     option("startingOffsets", "earliest"). \
     option("maxOffsetsPerTrigger", "1"). \
     load()
 
-schema = StructType(). \
+schema = StructType().\
     add('user_id', IntegerType()).\
-    add('item_id', IntegerType().\
+    add('item_id', IntegerType()).\
     add('quantity', IntegerType())
 
 value_als = als_data.select(F.from_json(F.col("value").cast("String"), schema).alias("value"), "offset")
@@ -47,12 +47,13 @@ def console_output(df, freq):
         .options(truncate=True) \
         .start()
 
-s = console_output(passengers_flat, 10)
+s = console_output(als_flat, 10)
 s.stop()
 
 ###############
 #подгружаем ML из HDFS
-pipeline_model = PipelineModel.load("/home/ksn38/models/als")
+#pipeline_model = PipelineModel.load("/home/ksn38/models/als")
+als_loaded = ALSModel.load("hdfs://bigdataanalytics2-head-shdpt-v31-1-0.novalocal:8020/user/305_kozik/models/als_loc")
 
 ##########
 #вся логика в этом foreachBatch
@@ -61,7 +62,7 @@ def writer_logic(df, epoch_id):
     print("---------I've got new batch--------")
     print("This is what I've got from Kafka:")
     df.show()
-    predict = pipeline_model.transform(df)
+    predict = als_loaded.transform(df)
     print("Here is what I've got after model transformation:")
     predict.show()
     #обновляем исторический агрегат в касандре
