050041902cd151b9426e2f54ba5a11f80f19f5b8
ksn38 <ksn38@yandex.ru>
2021-05-10 11:43:34 +0300

    -

--- trained_als_with_stream.py
+++ trained_als_with_stream.py
@@ -1,9 +1,8 @@
-#/kafka/bin/kafka-topics.sh --create --topic als_kafka --zookeeper 10.0.0.6:2181 --partitions 1 --replication-factor 2 --config retention.ms=-1
+#/kafka/bin/kafka-topics.sh --create --topic als_kafka2 --zookeeper 10.0.0.6:2181 --partitions 1 --replication-factor 2 --config retention.ms=-1
+#/kafka/bin/kafka-topics.sh --zookeeper 10.0.0.6:2181 --delete --topic als_kafka2
 
-#/kafka/bin/kafka-console-producer.sh --topic als_kafka --broker-list 10.0.0.6:6667
-
-#/kafka/bin/kafka-console-consumer.sh --topic als_kafka --bootstrap-server 10.0.0.6:6667
-
+#/kafka/bin/kafka-console-producer.sh --topic als_kafka2 --broker-list 10.0.0.6:6667
+#/kafka/bin/kafka-console-consumer.sh --topic als_kafka2 --bootstrap-server 10.0.0.6:6667
 
 #export SPARK_KAFKA_VERSION=0.10
 #/spark2.4/bin/pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.2 --driver-memory 512m --driver-cores 1 --master local[1]
@@ -26,7 +25,7 @@ kafka_brokers = "10.0.0.6:6667"
 als_data = spark.readStream. \
     format("kafka"). \
     option("kafka.bootstrap.servers", kafka_brokers). \
-    option("subscribe", "als_kafka"). \
+    option("subscribe", "als_kafka2"). \
     option("startingOffsets", "earliest"). \
     option("maxOffsetsPerTrigger", "1"). \
     load()
@@ -47,7 +46,7 @@ def console_output(df, freq):
         .options(truncate=True) \
         .start()
 
-s = console_output(als_flat, 10)
+s = console_output(als_flat, 3)
 s.stop()
 
 ###############
